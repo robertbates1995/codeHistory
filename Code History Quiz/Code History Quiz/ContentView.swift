43d9d64e14c048bbd1d6155cb0fb748da31280e5
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-08-15 18:12:21 -0700

    having issues with var body in contentView

--- Code History Quiz/Code History Quiz/ContentView.swift
+++ Code History Quiz/Code History Quiz/ContentView.swift
@@ -8,75 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+ 
+    let question = Question(
+        questionText: "What was the first computer bug?",
+        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
+        correctAnswerIndex: 2)
+ 
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+ 
     var body: some View {
-        NavigationView{
-            VStack {
-                Text("Items in Stock")
-                    .font(.title)
-                    .padding()
-                Spacer()
-                NavigationLink(
-                    destination: ItemDetailView(itemName: "Shrimp Chips"),
-                    label: {
-                        Text("Shrimp Chips")
-                    })
-                Spacer()
-                NavigationLink(
-                    destination: ItemDetailView(itemName: "Bueno"),
-                    label: {
-                        Text("Bueno")
-                    })
-                Spacer()
-            }
-            // Sets a navigation title of "Ligaya's Store"
-               .navigationTitle(Text("Bobby's Store"))
-               // Sets the styling of the navigation title to inline
-               .navigationBarTitleDisplayMode(.inline)
-        }
+        // View implementation
     }
 }
 
-struct ItemDetailView: View {
-    @State var quantityRemaining = Int.random(in: 1...10)
-    let itemName: String
-    
-    var body: some View {
-        VStack {
-            Text("\(itemName)")
-                .font(.title)
-                .padding()
-            Spacer()
-            Image(systemName: "photo")
-                .font(.system(size: 200))
-                .padding()
-            Text("Quantity Remaining: \(quantityRemaining)")
-            Spacer()
-            Button {
-                if quantityRemaining > 0 {
-                    quantityRemaining -= 1
-                }
-            } label: {
-                Text("Add one to your cart")
-            }
-            Spacer()
-        }
-        .background(
-              // The navigation link is not active until the quantity is 0
-              NavigationLink(destination: Text("You bought all the \(itemName)!"),
-                             isActive: .constant(quantityRemaining == 0),
-                             label: { EmptyView() })
-              )
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View{
-        ContentView()
-    }
-}
-
-struct ItemDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        ItemDetailView(itemName: "Test Item")
-    }
-}
---
+++ Code History Quiz/Code History Quiz/question.swift
@@ -0,0 +1,14 @@
+//
+//  question.swift
+//  Code History Quiz
+//
+//  Created by Robert Bates on 8/14/22.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
