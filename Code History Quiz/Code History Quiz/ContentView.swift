2adf2b3eddd2b2932b5064350c3e154c6fffc3b8
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-08-16 10:37:20 -0700

    made answer buttons modular

--- Code History Quiz/Code History Quiz/ContentView.swift
+++ Code History Quiz/Code History Quiz/ContentView.swift
@@ -14,20 +14,6 @@ struct ContentView: View {
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
     
-    //TODO: create generic form of button so I don't need to hardcode it every time.
-    /*
-    var questionButton: some Button {
-            action: {
-            print("Tapped on Choice 1")
-    }, label: {
-        Text("Ant")
-            .font(.body)
-            .bold()
-            .multilineTextAlignment(.center)
-            .padding()
-            .border(accentColor, width: 4)
-    }}
-     */
     
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
@@ -47,46 +33,18 @@ struct ContentView: View {
                         .multilineTextAlignment(.leading)
                     Spacer()
                     HStack {
-                        Button(action: {
-                            print("Tapped on \(question.possibleAnswers[0])")
-                        }, label: {
-                            Text("\(question.possibleAnswers[0])")
-                                .font(.body)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .border(accentColor, width: 4)
-                        })
-                        Button(action: {
-                            print("Tapped on \(question.possibleAnswers[1])")
-                        }, label: {
-                            Text("\(question.possibleAnswers[1])")
-                                .font(.body)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .border(accentColor, width: 4)
-                        })
-                        Button(action: {
-                            print("Tapped on \(question.possibleAnswers[2])")
-                        }, label: {
-                            Text("\(question.possibleAnswers[2])")
-                                .font(.body)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .border(accentColor, width: 4)
-                        })
-                        Button(action: {
-                            print("Tapped on \(question.possibleAnswers[3])")
-                        }, label: {
-                            Text("\(question.possibleAnswers[3])")
-                                .font(.body)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .border(accentColor, width: 4)
-                        })
+                        ForEach(question.possibleAnswers, id: \.self) { answer in
+                            Button(action: {
+                                print("Tapped on \(answer)")
+                            }, label: {
+                                Text("\(answer)")
+                                    .font(.body)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                    .border(accentColor, width: 4)
+                            })
+                        }
                 }
             }
             .foregroundColor(.white)
