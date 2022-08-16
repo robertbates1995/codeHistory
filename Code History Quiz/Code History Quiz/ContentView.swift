f808147e3b1edbc7e69f7bf44f52e79b9478c699
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-08-15 20:31:35 -0700

    added possible answer buttons

--- Code History Quiz/Code History Quiz/ContentView.swift
+++ Code History Quiz/Code History Quiz/ContentView.swift
@@ -13,14 +13,84 @@ struct ContentView: View {
         questionText: "What was the first computer bug?",
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
- 
+    
+    //TODO: create generic form of button so I don't need to hardcode it every time.
+    /*
+    var questionButton: some Button {
+            action: {
+            print("Tapped on Choice 1")
+    }, label: {
+        Text("Ant")
+            .font(.body)
+            .bold()
+            .multilineTextAlignment(.center)
+            .padding()
+            .border(accentColor, width: 4)
+    }}
+     */
+    
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
  
     var body: some View {
-        ZStack{
-            mainColor
+            ZStack {
+                mainColor.ignoresSafeArea()
+                VStack {
+                    Text("1 / 10")
+                        .font(.callout)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                        .padding()
+                    Text(question.questionText)
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                    HStack {
+                        Button(action: {
+                            print("Tapped on \(question.possibleAnswers[0])")
+                        }, label: {
+                            Text("\(question.possibleAnswers[0])")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                        Button(action: {
+                            print("Tapped on \(question.possibleAnswers[1])")
+                        }, label: {
+                            Text("\(question.possibleAnswers[1])")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                        Button(action: {
+                            print("Tapped on \(question.possibleAnswers[2])")
+                        }, label: {
+                            Text("\(question.possibleAnswers[2])")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                        Button(action: {
+                            print("Tapped on \(question.possibleAnswers[3])")
+                        }, label: {
+                            Text("\(question.possibleAnswers[3])")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                }
+            }
+            .foregroundColor(.white)
         }
-    }
 }
 
+}
